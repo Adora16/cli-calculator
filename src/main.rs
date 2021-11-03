4bb6b9f1d661bd999e97e9663e559f28608fe07e
Chii Aruel <chiiaruel@soulworker.cloudream>
2021-11-03 12:20:06 -0400

    Add user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io::{stdin, stdout};
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    let mut user_input = String::new();
+    print!("Input: ");
+    stdout().flush().unwrap();
+
+    stdin()
+        .read_line(&mut user_input)
+        .expect("Failed to read user input.");
+    println!("Your input was: {}", user_input);
 }
